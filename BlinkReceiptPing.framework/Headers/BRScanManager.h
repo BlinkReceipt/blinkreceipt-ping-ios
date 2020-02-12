fa068d68356b151d0a20ef74e45946d9e6f08c3d
Danny Panzer <danny.panzer@gmail.com>
2020-02-12 16:38:22 -0500

    v1.8.5 redo

--- BlinkReceiptPing.framework/Headers/BRScanManager.h
+++ BlinkReceiptPing.framework/Headers/BRScanManager.h
@@ -142,10 +142,11 @@
                 withCompletion:(nullable void(^)(BRScanResults* _Nullable, NSError* _Nullable))completion;
 
 /**
- * Retrieve resullts from disk for a specific receipt for custom user corrections flow
+ * Retrieve resullts from disk or remotely for a specific receipt for custom user corrections flow
  * @param blinkReceiptId    The receipt id to load from disk
- * @param completion
- *      * `BRScanResults `results` - The images associated with this `blinkReceiptId`
+ * @param completion    This callback is invoked once the SDK has attempted to retrieve results for this receipt
+ *      * `BRScanResults *results` - The scan results associated with this `blinkReceiptId`
+ *
  *      * `NSArray<UIImage*>* images` - The images associated with this `blinkReceiptId`
  *
  * @return The full scan results object for this receipt or `nil` if it could not be found
